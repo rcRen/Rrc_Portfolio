0cd8ed13a2380e88a12f674f3ac2ddf8c148cb6f
Rucheng Ren <rrc@p1.local>
2024-02-17 17:51:31 -0500

    ..

--- src/templates/project.js
+++ src/templates/project.js
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 import { Layout } from '../components';
 
-export default function Project({ data,location }) {
+export default function Project({ data, location }) {
     console.info(location)
     const { html } = data.markdownRemark;
     const { name, slogan, techStack, github, url } = data.markdownRemark.frontmatter
     return (
-        <Layout location={location}  className="bg-slate-100">
+        <Layout location={location} className="bg-slate-100">
             <div className='my-24 py-24 mx-48 space-y-8'>
                 <div className='flex space-x-3 items-center'>
                     <h1 className='text-4xl font-semibold'>{name}:</h1><span className='text-2xl font-semibold text-gray-500'>{slogan}</span>
                 </div>
                 <div className='flex flex-row space-x-3'>
-                    <a className="font-bold relative w-fit block after:block after:content-[''] after:absolute after:h-1 after:bg-orange-800 after:w-full after:scale-x-0 hover:text-orange-800 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left" href={github}><img className='w-6 h-6 inline align-middle' src='/icons/github.svg' />GITHUB REPO</a>
+                    <a className="font-bold relative w-fit block after:block after:content-[''] after:absolute after:h-1 after:bg-orange-800 after:w-full after:scale-x-0 hover:text-orange-800 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left" href={github}><img className='w-6 h-6 inline align-middle' src={`${__PATH_PREFIX__}/icons/github.svg`} />GITHUB REPO</a>
                     <a className="font-bold relative w-fit block after:block after:content-[''] after:absolute after:h-1 after:bg-orange-800 after:w-full after:scale-x-0 hover:text-orange-800 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left" href={url}><img className='w-6 h-6 inline align-middle' src='/icons/link.svg' />LIVE DEMO</a></div>
                 <div className="flex space-x-3">
                     {techStack.map((tech, index) => (
